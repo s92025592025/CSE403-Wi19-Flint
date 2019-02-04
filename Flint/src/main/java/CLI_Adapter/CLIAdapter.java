33bb795672908ee4157b6488f5eb467af479e46b
Daniel Wang <s92025592025@gmail.com>
2019-02-04 15:05:12 -0800

    setup function headers

--- Flint/src/main/java/CLI_Adapter/CLIAdapter.java
+++ Flint/src/main/java/CLI_Adapter/CLIAdapter.java
@@ -1,4 +1,26 @@
 package CLI_Adapter;
 
+import flint.FlintConfiguration;
+
 public class CLIAdapter {
+    public static void main(String[] args) throws Exception {
+        throw new Exception("Not Yet Implemented");
+    }
+
+    private static FlintConfiguration configInit(String configPath) throws Exception {
+        throw new Exception("Not Yet Implemented");
+    }
+
+    private static boolean isJava(String filePath) throws Exception {
+        throw new Exception("Not Yet Implemented");
+    }
+
+    private static Collection<Failure> run(String fileToLint, FlintConfiguration config) throws Exception{
+        throw new Exception("Not Yet Implemented");
+    }
+
+    private String resultOutput(Collection<Failure> result) throws Exception{
+        throw new Exception("Not Yet Implemented");
+    }
+
 }
