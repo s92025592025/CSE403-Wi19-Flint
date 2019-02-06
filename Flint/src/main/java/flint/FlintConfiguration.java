c5f77770a3528fdd1b4d03dda7bfa0fd48011682
Ofek Inbar <ofekinbar@hotmail.com>
2019-02-05 18:43:40 -0800

    Use RandomAccessFile instead of Scanner

--- Flint/src/main/java/flint/FlintConfiguration.java
+++ Flint/src/main/java/flint/FlintConfiguration.java
@@ -1,12 +1,13 @@
 package flint;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.Collection;
-import java.util.Scanner;
 
 import com.github.javaparser.ast.CompilationUnit;
 
 public abstract class FlintConfiguration {
-  public Collection<LintFailure> runChecks(Scanner inputFile, CompilationUnit astRoot) {
+  public Collection<LintFailure> runChecks(RandomAccessFile inputFile, CompilationUnit astRoot) throws IOException {
     System.out.println("Got here!");
     return null;
   }
--- Flint/src/main/java/flint/FlintDriver.java
+++ Flint/src/main/java/flint/FlintDriver.java
@@ -2,30 +2,36 @@ package flint;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.Collection;
-import java.util.Scanner;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseProblemException;
 
 public class FlintDriver {
   /**
-   * This method checks whether fileToLint refers to a compileable Java class, throwing
-   * an IllegalArgumentException if not, then produces a Scanner to the file as
-   * well as a CompilationUnit (the result of parsing the file and the root of an
-   * Abstract Syntax Tree), which it passes to config.
+   * This method checks whether fileToLint refers to a compileable Java class,
+   * throwing an IllegalArgumentException if not, then produces a Scanner to the
+   * file as well as a CompilationUnit (the result of parsing the file and the
+   * root of an Abstract Syntax Tree), which it passes to config.
+   * 
    * @param fileToLint - Filepath to the file we wish to lint
-   * @param config - Configuration file that defines how to run the checks
+   * @param config     - Configuration file that defines how to run the checks
    * @return Every LintFailure resulting from running the checks in config
-   * @throws FileNotFoundException - If fileToLint does not refer to a valid file
+   * @throws FileNotFoundException    - If fileToLint does not refer to a valid
+   *                                  file
    * @throws IllegalArgumentException - If fileToLint does not compile
-   * @throws ParseProblemException - If fileToLint could not be successfully parsed
+   * @throws ParseProblemException    - If fileToLint could not be successfully
+   *                                  parsed
    */
-  public static Collection<LintFailure> run(String fileToLint, FlintConfiguration config) 
-      throws FileNotFoundException, IllegalArgumentException, ParseProblemException {
+  public static Collection<LintFailure> run(String fileToLint, FlintConfiguration config)
+      throws FileNotFoundException, IllegalArgumentException, ParseProblemException, IOException {
     if (doesCompile(fileToLint)) {
-      File inputFile = new File(fileToLint);
-      return config.runChecks(new Scanner(inputFile), JavaParser.parse(inputFile));
+      RandomAccessFile inputFile = new RandomAccessFile(fileToLint, "r");
+      Collection<LintFailure> result = config.runChecks(inputFile, JavaParser.parse(new File(fileToLint)));
+      inputFile.close();
+      return result;
     } else {
       throw new IllegalArgumentException("Filepath provided does not compile!");
     }
