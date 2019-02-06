03a5b3f6250a6480abb8cb1a027c4fee99ed6bf8
pxc2 <pxc2@uw.edu>
2019-02-06 14:13:52 -0800

    edit FlintConfig

--- Flint/src/main/java/flint/FlintConfiguration.java
+++ Flint/src/main/java/flint/FlintConfiguration.java
@@ -1,5 +1,25 @@
 package flint;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Collection;
+
+import com.github.javaparser.ast.CompilationUnit;
+
 public abstract class FlintConfiguration {
-  
+  CompilationUnit astRoot;
+  RandomAccessFile inputFile;
+  Collection<FlintRule> rules;
+  Collection<LintFailure> failures;
+
+  public FlintConfiguration(CompilationUnit astRoot, RandomAccessFile inputFile);
+
+  /**
+   * Defines how to run whatever rules it chooses to run (in what order, under what conditions, etc), 
+   * passes the CompilationUnit astRoot and RandomAccessFile inputFile to each rule, 
+   * runs checks, and returns the collection of consequent failures to the caller.
+   *
+   * @return Returns a collection of consequent LintFailure
+   * */
+  public abstract Collection<LintFailure> runChecks();
 }
