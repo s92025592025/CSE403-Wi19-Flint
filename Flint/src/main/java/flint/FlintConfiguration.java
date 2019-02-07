de72ef776aed65a28799c8ec995e02f900f82483
pxc2 <pxc2@uw.edu>
2019-02-06 23:50:57 -0800

    removed fiels, modified cotr, edited runchecks signature

--- Flint/src/main/java/flint/FlintConfiguration.java
+++ Flint/src/main/java/flint/FlintConfiguration.java
@@ -7,19 +7,19 @@ import java.util.Collection;
 import com.github.javaparser.ast.CompilationUnit;
 
 public abstract class FlintConfiguration {
-  CompilationUnit astRoot;
-  RandomAccessFile inputFile;
-  Collection<FlintRule> rules;
-  Collection<LintFailure> failures;
 
-  public FlintConfiguration(CompilationUnit astRoot, RandomAccessFile inputFile);
+  public FlintConfiguration();
 
   /**
-   * Defines how to run whatever rules it chooses to run (in what order, under what conditions, etc), 
+   * Defines how to run whatever rules it chooses to run 
+   * (in what order, under what conditions, etc), 
    * passes the CompilationUnit astRoot and RandomAccessFile inputFile to each rule, 
    * runs checks, and returns the collection of consequent failures to the caller.
    *
+   * @param astRoot - the root of the abstract syntax tree of the input file
+   * @param inputFile - the input file to be run checks on
    * @return Returns a collection of consequent LintFailure
    * */
-  public abstract Collection<LintFailure> runChecks();
+  public abstract Collection<LintFailure> runChecks(CompilationUnit astRoot, 
+                                                    RandomAccessFile inputFile);
 }
