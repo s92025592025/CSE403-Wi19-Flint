c054ec6bce343067f6baf57102655c4eaaaa35a0
Ofek Inbar <ofekinbar@hotmail.com>
2019-02-10 13:01:38 -0800

    Fix build errors

--- Flint/src/main/java/flint/FlintConfiguration.java
+++ Flint/src/main/java/flint/FlintConfiguration.java
@@ -7,15 +7,15 @@ import com.github.javaparser.ast.CompilationUnit;
 
 public abstract class FlintConfiguration {
   /**
-   * Defines how to run whatever rules it chooses to run 
-   * (in what order, under what conditions, etc), 
-   * passes the CompilationUnit astRoot and RandomAccessFile inputFile to each rule, 
-   * runs checks, and returns the collection of consequent failures to the caller.
+   * Defines how to run whatever rules it chooses to run (in what order, under
+   * what conditions, etc), passes the CompilationUnit astRoot and
+   * RandomAccessFile inputFile to each rule, runs checks, and returns the
+   * collection of consequent failures to the caller.
    *
-   * @param astRoot - the root of the abstract syntax tree of the input file
+   * @param astRoot   - the root of the abstract syntax tree of the input file
    * @param inputFile - the input file to be run checks on
    * @return Returns a collection of consequent LintFailure
-   * */
-  public abstract Collection<LintFailure> runChecks(CompilationUnit astRoot, 
-                                                    RandomAccessFile inputFile);
+   */
+  public abstract Collection<LintFailure> runChecks(CompilationUnit astRoot, RandomAccessFile inputFile)
+      throws Exception;
 }
