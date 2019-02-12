05110393c068a4f596738b7b76d67280bec7203b
Daniel Wang <s92025592025@gmail.com>
2019-02-12 11:48:02 -0800

    can tell when there is no flint-<version>.jar in same dir

--- scripts/flint.sh
+++ scripts/flint.sh
@@ -2,4 +2,13 @@
 
 CURRENT_DIR="${0%/*}"
 
-java -jar "${CURRENT_DIR}/flint-0.1.3.jar" "$@"
+
+if [ 0 -lt $(ls -l "${CURRENT_DIR}" | grep flint-.*.jar | wc -l) ]; 
+then
+	NEWEST_FLINT="$(ls "${CURRENT_DIR}" | grep /flint-*.jar | sort -t- -k2 -V -r | head -1)"
+	echo "$NEWEST_FLINT"
+	java -jar "${CURRENT_DIR}/flint-0.1.3.jar" "$@"
+else
+	echo "flint-<version>.jar should be in the same directory as flint.sh"
+fi
+
